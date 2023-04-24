fcc9150d4db31dc0cdb986d24cbb75e264f297b5
insh007 <aliinshad9546@gmail.com>
2023-04-25 00:40:05 +0530

    adding worker_threads

---
+++ app.js
@@ -0,0 +1,25 @@
+const {Worker ,isMainThread, workerData, parentPort } = require('worker_threads')
+
+if(isMainThread){
+    const express = require('express')
+    const app = express()
+
+    app.get("/light", (req, res) => {
+        res.send(`Request completed`);
+    })
+
+    app.get("/heavy", (req, res) => {
+        const worker = new Worker(__filename, { workerData: "workerOne" })
+        worker.on("message", (data) => {
+            res.send(data)
+        })
+    })
+
+    app.listen(3000, () => console.log("Listening to port 3000"))
+}else{
+    let counter = 0;
+    while (counter < 9000000000) {
+        counter++;
+    }
+    parentPort.postMessage(`${counter} iterations completed by ${workerData}`)
+}
